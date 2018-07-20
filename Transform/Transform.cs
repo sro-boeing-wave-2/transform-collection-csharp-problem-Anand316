5120e375d141c767c505ef31913289922d2a68b1
Anandkumar <anandkumar316sw@gmail.com>
2018-07-20 14:07:52 +0530

    Assign-6 Solution

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -2,13 +2,23 @@
 using System.Linq;
 using System.Collections.Generic;
 
+
 namespace Transform
 {
+    public delegate int mapdel(int y);
+
     public static class Transform 
     {
-        public static void Map()
+        public static List<int> Map(this int[] j, mapdel operation)
         {
-            throw new NotImplementedException();
+            List<int> trans = new List<int>();
+            foreach (int n in j)
+            {
+                trans.Add(operation(n));
+            }
+            return trans;
+
+            //throw new NotImplementedException();
         }
     }
 }
